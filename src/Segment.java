664060ebd650056d7e8e5dba5d5f410b3295c430
Volt <sonianadashkevich@gmail.com>
2019-10-11 13:34:44 +0200

    segment constructor added

--- src/Segment.java
+++ src/Segment.java
@@ -2,6 +2,11 @@ class Segment {
     private Point a;
     private Point b;
 
+    Segment(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+    }
+
     public Point getA() {
         return a;
     }
